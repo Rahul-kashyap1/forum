3956d914da8e510a13da36546a01898eb19101f9
Rahul-kashyap1 <71591867+Rahul-kashyap1@users.noreply.github.com>
2021-10-16 18:58:10 +0530

    Add files via upload

---
+++ partials/_dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+$servername="sql303.epizy.com";
+$username="epiz_30082879	";
+$password="QLhIZhGYpVKDD";
+$database="epiz_30082879_idiscuss";
+$conn=mysqli_connect($servername,$username,$password,$database);
+
+?>
